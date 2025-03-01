283b1f73565c39955baa42d059bf33acc4935318
Cy_Wilson <youremail@example.com>
2025-03-01 19:54:31 +0800

    make the rock paper scissors game with classes

--- rock-paper-scissors/program.cpp
+++ rock-paper-scissors/program.cpp
@@ -1,82 +1,100 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
-  srand(time(0)); //seed random number generator
+//PROTECTED VARIABLES
+class Main
+{
+  protected:
+    string rock = "rock";
+    string paper = "paper";
+    string scissors = "scissors";
+    string computerMove;
+    string userMove;
+    char playAgain = 'y';
+};
 
-  //initialization of variables, computer move, user move, and play again
-  string rock = "rock";
-  string paper = "paper";
-  string scissors = "scissors";
-  string computerMove;
-  string userMove;
+//GAME CLASS INHERITING MAIN CLASS TO RENDER GAME LOGIC
+class Game : public Main
+{
+  private:
+  void aiMove(){
+    srand(time(0));
 
-  char playAgain = 'y';
+    int randomMove = rand() % 3 + 1;
+    if (randomMove == 1) {
+        computerMove = rock;
+    } else if (randomMove == 2) {
+        computerMove = paper;
+    } else {
+        computerMove = scissors;
+    }
+  }
 
-  //GAME LOGIC, do while loop for user to play again
-  do
-  {
-    cout << "Choose, rock, paper, or scissors?" << endl;
-    cin >> userMove;
-  
-      int randomMove = rand() % 3 + 1;
-      if (randomMove == 1) {
-          computerMove = rock;
-          }
-      else if (randomMove == 2) {
-          computerMove = paper;
-          }
-      else {
-          computerMove = scissors;
-          }
-      
-      //user move vs computer move
-      if (userMove == computerMove) {
-          cout << "It's a tie! You both chose " << userMove << endl;
-      } else if (userMove == computerMove) {
+  //GAME LOGIC 
+  public:
+    void gameLogic(){
+      cout << "Welcome to Rock, Paper, Scissors!" << endl;
+      cout << "==============================================" << endl;
+      cout << "You will be playing against the computer." << endl;
+      cout << "==============================================" << endl;
+
+      do
+      {
+        cout << "Enter your move (rock, paper, or scissors): " << endl;
+        cin >> userMove;
+        aiMove();
+
+       if (userMove == computerMove) {
           cout << "You chose " << userMove << " and the computer chose " << computerMove << ". It's a tie!" << endl;
-      } else if (userMove == rock) {
+          cout << "==============================================" << endl;
+        } else if (userMove == rock) {
           if (computerMove == paper) {
               cout << "You chose " << userMove << " and the computer chose " << computerMove << ". Paper wins!" << endl;
+              cout << "==============================================" << endl;
           } else {
               cout << "You chose " << userMove << " and the computer chose " << computerMove << ". Rock wins!" << endl;
+              cout << "==============================================" << endl;
           }
-      } else if (userMove == paper) {
+        } else if (userMove == paper) {
           if (computerMove == rock) {
               cout << "You chose " << userMove << " and the computer chose " << computerMove << ". Paper wins!" << endl;
+              cout << "==============================================" << endl;
           } else {
               cout << "You chose " << userMove << " and the computer chose " << computerMove << ". Scissors wins!" << endl;
+              cout << "==============================================" << endl;
           }
-      } else if (userMove == scissors) {
+        } else if (userMove == scissors) {
           if (computerMove == rock) {
               cout << "You chose " << userMove << " and the computer chose " << computerMove << ". Rock wins!" << endl;
+              cout << "==============================================" << endl;
           } else {
               cout << "You chose " << userMove << " and the computer chose " << computerMove << ". Scissors wins!" << endl;
+              cout << "==============================================" << endl;
           }
-      } else {
+        } else {
           cout << "Invalid move. Please enter rock, paper, or scissors." << endl;
-      }
+          cout << "==============================================" << endl;
+        }
 
-      cout << "Do you want to play again? (y/n): ";
-      cin >> playAgain;
+        cout << "Do you want to play again? (y/n): ";
+        cin >> playAgain;
+        if(playAgain == 'n'){
+          break;
+        } else{
+          cout << "Get ready to play again" << endl;
+          cout << "==============================================" << endl;
+        }
+      } while (playAgain == 'y');
+    }
+};
 
-      //Prompt if user didn't type rock, paper, or scissors
-      while(userMove != rock && userMove != paper && userMove != scissors){
-          cout << "Enter your move (rock, paper, or scissors): ";
-          cin >> userMove;
-      }
-
-      //Prompt if user didn't type y and n
-      while(playAgain != 'y' && playAgain != 'n'){
-          cout << "Do you want to play again? (y/n): ";
-          cin >> playAgain;
-      }
-  } while (playAgain == 'y');
-  
-
-  //EXIT NA
+//MAIN FUNCTION
+int main(){
+  Game game;
+  game.gameLogic();
   cout << "================================================================" << endl;
   cout << "Thanks for playing!" << endl;
   return 0;
